f91819dc5d80ea93fd2af9e7d88ed54e92373efb
Haikel Fadhloun <51424729+haikelfd@users.noreply.github.com>
2020-11-07 16:32:56 -0800

    EAA V01

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
 	private String firstName ;
 	private String lastName ;
@@ -19,7 +21,13 @@ public class Email {
 	
 	// Ask for the department 
 	private String setDepartment() {
-		
+		System.out.print(" Enter the departement\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none");
+		Scanner in = new Scanner(System.in);
+		int  depChoice = in.nextInt();
+		if(depChoice ==1) { return "sales";}
+		else if (depChoice == 2) { return "dev" ;}
+		else if (depChoice == 3) { return "acct";}
+		else {return "";}
 	}
 	
 	
